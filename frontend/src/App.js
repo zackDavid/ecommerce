31a3841d962a0de0ca7922af7d9d71273dd6ced7
zackDavid <92186526+zackDavid@users.noreply.github.com>
2026-09-07 12:19:57 -0600

    feat: integrar componente de HomeScreen en App.js para que se vean los productos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import {Container, Row, Col} from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
@@ -8,7 +10,7 @@ const App = () => {
   <Header />
     <main className='py-3'>
       <Container>
-        <h1>Bienvenido al ecommerce</h1>
+        <HomeScreen />
       </Container>
     </main>
     <Footer />
