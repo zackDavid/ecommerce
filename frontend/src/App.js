fb332255e6b1b5bc7ed9fe57f590f55b70edfe1e
zackDavid <92186526+zackDavid@users.noreply.github.com>
2026-09-07 12:50:19 -0600

    feat: remover componente HomeScreen por Outlet para enrutamiento en App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
+
 
 const App = () => {
   return (
@@ -10,7 +10,7 @@ const App = () => {
   <Header />
     <main className='py-3'>
       <Container>
-        <HomeScreen />
+        <Outlet />
       </Container>
     </main>
     <Footer />
