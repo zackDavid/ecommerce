da9d5ea0a3cee483bf6862abf127a5307a6a36c4
zackDavid <92186526+zackDavid@users.noreply.github.com>
2026-09-07 12:20:47 -0600

    feat: agregar prop product al componente Product para mostrar detalles del producto

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
 import {Card} from 'react-bootstrap';
-import Link from 'react-router-dom';
 
 
-const Product = () => {
+
+const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
         <a href={`/product/${product._id}`}>
             <Card.Img src={product.image} variant='top' />
         </a>
+
         <Card.Body>
             <a href={`/product/${product._id}`}>    
                 <Card.Title as='div'>
@@ -18,9 +18,6 @@ const Product = () => {
 
             <Card.Text as='h3'>${product.price}</Card.Text>
         </Card.Body>
-
-
-
     </Card>
   )
 }
