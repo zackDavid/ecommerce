19c61d32ffd5fba5081375260814019f7bab90cf
zackDavid <92186526+zackDavid@users.noreply.github.com>
2026-09-09 13:10:30 -0600

    refactor: fetch desde useEffect y useState

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,58 +1,75 @@
+import {useEffect, useState} from 'react';
 import  {Link, useParams} from 'react-router-dom';
 import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
-
+import axios from 'axios';
 
 const ProductScreen = () => {
 
-    const { id: productId } = useParams();
-    const product = products.find((p) => p._id === productId);
-    console.log(product);
+  const [product, setProducts] = useState([]);
 
-  return (
-    <>
-    <Link className='btn btn-light my-3' to='/'>Regresar</Link>
-      <Row>
-        <Col md={5}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
-      
-        <Col md={4}>
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-          </ListGroup.Item>
-            <ListGroupItem>
-              <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-            </ListGroupItem>
-        </Col>
+    const {id: productId} = useParams();
+    useEffect(() => {
+      const fetchProduct = async () => {
+        const { data } = await axios.get(`/api/products/${productId}`);
+        setProducts(data);
+      }
 
-      <Col md={3}>
-          <Card>
+      fetchProduct();
+    }, [productId]);
+
+    return (
+      <>
+      <Link className='btn btn-light my-3' to='/'>Regresar</Link>
+        <Row>
+          <Col md={5}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
+          <Col md={4}>
             <ListGroup variant='flush'>
               <ListGroup.Item>
-                <Row>
-                  <Col>Estatus:</Col>
-                  <Col>
-                    <strong>${product.countInStock > 0 ? 'En Stock' : 'Fuera de Stock'}</strong>
-                  </Col>
-                </Row>
+                <h3>{product.name}</h3>
               </ListGroup.Item>
-              <ListGroup.Item>
-                <Button
-                  className='btn-block'
-                  type='button'
-                  disabled={product.countInStock === 0}
-                >
-                  Agregar al Carrito
-                </Button>
-              </ListGroup.Item>
+              <ListGroupItem>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+              </ListGroupItem>
+              <ListGroupItem>Precio: ${product.price}</ListGroupItem>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
-    </>
-  );
-};
+          </Col>
+          <Col md={3}>
+              <Card>
+                <ListGroup variant='flush'>
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Precio:</Col>
+                      <Col>
+                        <strong>${product.price}</strong>
+                      </Col>
+                    </Row>
+                    </ListGroup.Item>                    
+                    <ListGroup.Item>
+                    <Row>
+                      <Col>Estatus:</Col>
+                      <Col>
+                        <strong>${product.countInStock > 0 ? 'En Stock' : 'Fuera de Stock'}</strong>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                  <ListGroup.Item>
+                    <Button
+                      className='btn-block'
+                      type='button'
+                      disabled={product.countInStock === 0}
+                    >
+                      Agregar al Carrito
+                    </Button>
+                  </ListGroup.Item>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
+      </>
+    );
+  };
 
 export default ProductScreen;
