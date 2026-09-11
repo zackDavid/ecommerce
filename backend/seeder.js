35b79f76f3cad3e7cbb4a03a57065057530374df
zackDavid <92186526+zackDavid@users.noreply.github.com>
2026-09-11 11:28:23 -0600

    feat: seeder para inicializar y destruir datos

---
+++ backend/seeder.js
@@ -0,0 +1,50 @@
+import mogoose from 'mongoose';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import users from "./data/users.js";
+import Product from "./models/productModel.js"
+import Order from "./models/orderModel.js"
+import User from "./models/userModel.js"
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+    try {
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        const createdUsers = await User.inserMany(users);
+        
+        const adminUser = createdUsers[0]._id;
+
+        const sampleProdcts = products.map((product) => {
+            return { ...product, user: adminUser }; 
+        });
+
+        await Product.insertMany(sampleProdcts);
+
+        console.log('Data Imported!'.green.inverse);
+        process.exit();
+    } catch (error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1);
+    }
+}
+
+const destroyData = async () => {
+    try {
+        await Order.deleteMany();
+        await Product.deleteMany();
+        await User.deleteMany();
+
+        console.log('Data Destroyed!'.red.inverse);
+        process.exit();
+    } catch (error) {
+        console.error(`${error}`.red.inverse);
+        process.exit(1);
+    }
+};
