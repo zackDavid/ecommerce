c0757a312d738bbaaedd2bb386158db98dbf08a5
zackDavid <92186526+zackDavid@users.noreply.github.com>
2026-09-10 18:25:38 -0600

    feat: modelo de usuarios

---
+++ backend/config/models/userModel.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        tupe: String,
+        required: true,
+
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false,
+    },
+    password: {
+        type: String,
+        required: true,
+
+    },
+
+}, { 
+    timestamps: true,
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
