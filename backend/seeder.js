fd383f2bc6649b65822d207688a743cccb0cfbc0
zackDavid <92186526+zackDavid@users.noreply.github.com>
2026-09-11 12:11:09 -0600

    feat: import de productos

--- backend/seeder.js
+++ backend/seeder.js
@@ -2,6 +2,7 @@ import mogoose from 'mongoose';
 import dotenv from 'dotenv';
 import colors from 'colors';
 import users from "./data/users.js";
+import products from "./data/products.js";
 import Product from "./models/productModel.js"
 import Order from "./models/orderModel.js"
 import User from "./models/userModel.js"
