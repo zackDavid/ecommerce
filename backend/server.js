0581499a103763b5a21d9c90a6d795f4c07a14c8
zackDavid <92186526+zackDavid@users.noreply.github.com>
2026-09-11 18:15:57 -0600

    feat: añadir rutas de productos al servidor

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
 const port = process.env.PORT || 5000;
+import productRoutes from './routes/productRoutes.js';
+
 
 connectDB();
 
@@ -13,6 +15,6 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
