11e66da8ed54567162b8b3e6d0b4a2a53893fec0
zackDavid <92186526+zackDavid@users.noreply.github.com>
2026-09-14 12:36:36 -0600

    refactor: actualizacion de estatus de errores

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,17 +9,19 @@ router.get('/', asyncHandler(async (req, res) => {
     res.json(products);
 }));
 
-router.get('/:id', asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
+router.get(
+    '/:id', 
+    asyncHandler(async (req, res) => {
+        const product = await Product.findById(req.params.id);
 
-    //mensaje de error en caso de que el producto no exista
-    if(product) {
-        return res.json(product); 
-    }
-
-    res.status(404).json({message: 'Product not found'})
-
-    res.json(product);
-}));
+        //mensaje de error en caso de que el producto no exista
+        if(product) {
+            return res.json(product); 
+        } else {
+            res.status(404);
+            throw new Error('Resource not found');
+        }
+    })
+);
 
 export default router;
