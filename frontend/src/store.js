085dc2debaf0d92d868f8b580b039868c06de874
zackDavid <92186526+zackDavid@users.noreply.github.com>
2026-09-15 14:36:32 -0600

    refactor: espacio extra

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,8 +5,7 @@ const store = configureStore({
     reducer: {
         [apiSlice.reducerPath]: apiSlice.reducer,
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat
-    (apiSlice.middleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
     devTools: true,
 
 });
