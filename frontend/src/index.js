3238234519e86fd84ce885bc04bc5dd4b1e971fb
zackDavid <92186526+zackDavid@users.noreply.github.com>
2026-09-15 10:40:43 -0600

    fix: comentario

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -27,7 +27,7 @@ const router = createBrowserRouter(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    //Wrapper de estado de Redux
+
     <Provider store={store}>
     <RouterProvider router={router} />
     </Provider>
